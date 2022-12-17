74421a00f39a166374995d8467008fd3d4de79e7
atom-smasher <atom@smasher.org>
2022-12-17 14:28:24 +1300

    v1.0h
    Less forking, more helpful error messages, minor bug fixes.

--- vsleep.sh
+++ vsleep.sh
@@ -3,7 +3,7 @@
 #######################################
 ## atom smasher's vsleep: verbose sleep
 ## v1.0  12 dec 2022
-## v1.0h 15 dec 2022
+## v1.0h 17 dec 2022
 ## Distributed under the GNU General Public License
 ## http://www.gnu.org/copyleft/gpl.html
 
@@ -18,14 +18,16 @@
 show_help () {
     echo 'usage:'
     echo '  vsleep [OPTIONS] DELAY|TARGET'
-    echo '    DELAY = sleep this many seconds'
+    echo '    DELAY = sleep this many seconds (integer)'
     echo '    TARGET = sleep until this time (formats supported by DATE STRING)'
-    echo '    -j JITTER = randomly add up to this many seconds to the DELAY or TARGET time'
-    echo '    -J JITTER = randomly add or subtract up to this many seconds to or from the DELAY or TARGET time'
+    echo '    -j JITTER = randomly add up to JITTER seconds to the DELAY or TARGET time'
+    echo '    -J JITTER = randomly add or subtract up to JITTER seconds to or from the DELAY or TARGET time'
+    echo '      * JITTER must be specified as an integer'
     echo '    -p = show progress bar (off by default)      (pv option --progress)'
     echo '    -E = disable countdown timer (on by default) (pv option --eta)'
     echo '    -I = disable ETA time (on by default)        (pv option --fineta)'
     echo '    -q = no output from pv                       (pv option --quiet)'
+    exit ${1}
 }
 
 ## jitter function
@@ -33,8 +35,17 @@ calc_random_jitter () {
     shuf -i 1-${1} -n 1
 }
 
+test_jitter_integer () {
+    case "${1}" in
+	*[!0-9]*)
+	    echo "${0##*/}: error: '${*}' JITTER must be specified as an integer"
+	    show_help 3
+	    ;;
+    esac
+}
+
 ## unset these variables; they'll be set later, if needed
-unset jitter_add jitter_plus_minus progress_bar pv_quiet time_fmt
+unset jitter_add jitter_plus_minus progress_bar pv_quiet target_date
 
 ## set these variables; they'll be unset later, if needed
 pv_eta='--eta'
@@ -46,10 +57,12 @@ do
     case ${options} in
 	j)
 	    ## specify a random delay, in addition to specified delay/target
+	    test_jitter_integer ${OPTARG}
 	    jitter_add=$(calc_random_jitter ${OPTARG})
 	    ;;
 	J)
 	    ## specify a random delay, plus or minus specified delay/target
+	    test_jitter_integer ${OPTARG}
    	    jitter_plus_minus=$(tr -dc '+-' < /dev/urandom | head -c 1 ; calc_random_jitter ${OPTARG})
 	    ;;
 	p)
@@ -68,16 +81,20 @@ do
 	    pv_quiet='--quiet'
 	    ;;
 	h)
-	    show_help
-	    exit
+	    show_help 0
 	    ;;
 	?)
-	    show_help
-	    exit 1
+	    show_help 1
 	    ;;
     esac
 done
 shift $(( $OPTIND - 1 ))
+
+## test for empty DELAY|TARGET
+[ 1 -gt ${#*} ] && {
+    echo "${0##*/}: error: DELAY|TARGET must be specified"
+    show_help 4
+}
 
 ## if pv's --eta and --fineta are both turned off, it defaults to showing progress
 ## turn that off, unless it's explicitly turned on
@@ -86,25 +103,27 @@ shift $(( $OPTIND - 1 ))
 ## if "DELAY|TARGET" contains non-numeric characters, process it as a TARGET
 ## this case construct tests whether the delay|target argument should be treated as a delay or target
 ## without forking a grep
-case "${*}"
-in
-    *[^0-9]*)
-	time_fmt='target'
-    ;;
+case "${*}" in
+    *[!0-9]*)
+	## santity check, if TARGET is valid
+	## here, fork 'date' to interpret the TARGET, and store that in a variable so it can be re-used without another fork
+	target_date=$( date -d "${*}" +%s 2> /dev/null ) || {
+	    ## test if that 'date' fails
+	    echo "${0##*/}: error: '${*}' TARGET is not valid"
+	    show_help 2
+	}
+	## calculate a "wait until time"
+	delay=$(( ${target_date} - $( date +%s ) - 1 ))
+	## wait ; this waits until the next clock second, before starting the countdown
+	## not ideal, but it tends to give much more precise execution time
+	## this also seems to be a necessary evil, to get pv to display the correct ETA
+	sleep $( printf "0.%0.9d" $(( 1000000000 - $(date +%-N) )) )
+	;;
+    *)
+	## here, $delay just equals the seconds, as specified as input
+	delay=${1}
+	;;
 esac
-
-[ 'target' = "${time_fmt}" ] && {
-    ## calculate a "wait until time", if needed
-    delay=$(( $( date -d "${*}" +%s  ) - $( date +%s ) - 1 ))
-    ## wait ; this waits until the next clock second, before starting the countdown
-    ## not perfect, but it tends to give much more precise execution time
-    ## this also seems to be a necessary evil, to get pv to display the correct ETA
-    sleep $( printf "0.%0.9d" $(( 1000000000 - $(date +%-N) )) )
-} || {
-    ## or else, delay equals seconds as specified as an argument
-    ## if "DELAY|TARGET" contains only numeric characters, process it as a DELAY
-    delay=${1}
-}
 
 ## add jitter, if specified
 [ "${jitter_add}" ] && delay=$(( ${delay} + ${jitter_add} ))
@@ -121,3 +140,4 @@ esac
 ## at the heart of this script is a yes/pv trick that I found here, and significantly expanded on -
 ## https://unix.stackexchange.com/questions/600868/verbose-sleep-command-that-displays-pending-time-seconds-minutes
 yes | pv ${progress_bar} ${pv_eta} ${pv_eta_fine} ${pv_quiet} --rate-limit 10 --stop-at-size --size $(( ${delay} * 10 )) > /dev/null
+
